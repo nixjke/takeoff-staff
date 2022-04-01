17350bfd5e3bf9c416e543f07f53d46fd9f827e1
SosiskaPish <aziz.10.03@mail.ru>
2022-04-01 14:44:55 +0300

    init persist

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,16 @@
 import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
-import { store } from './store/store'
+import { store, persistor } from './store/store'
 import { Provider } from 'react-redux'
+import { PersistGate } from 'redux-persist/integration/react'
 import * as serviceWorker from './serviceWorker'
 
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <PersistGate loading={null} persistor={persistor}>
+      <App />
+    </PersistGate>
   </Provider>,
   document.getElementById('root')
 )
--- src/store/store.ts
+++ src/store/store.ts
@@ -1,11 +1,31 @@
-import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit'
+import { configureStore, combineReducers, ThunkAction, Action } from '@reduxjs/toolkit'
+import { persistStore, persistReducer, FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER } from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
 import userSlice from './slices/userSlice'
 
-export const store = configureStore({
-  reducer: {
-    user: userSlice
-  }
+const persistConfig = {
+  key: 'root',
+  storage,
+  whitelist: ['user']
+}
+
+const rootReducer = combineReducers({
+  user: userSlice
 })
+
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+
+export const store = configureStore({
+  reducer: persistedReducer,
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER]
+      }
+    })
+})
+
+export let persistor = persistStore(store)
 
 export type AppDispatch = typeof store.dispatch
 export type RootState = ReturnType<typeof store.getState>
